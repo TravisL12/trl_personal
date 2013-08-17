a3119d428c14f0cc35512beff0f66411f62b6ee4
Travisl12 <travis.lawrence12@gmail.com>
2013-08-16 19:55:24 -0700

    file cleanup

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-p # Set ugems listed in the Gemfile.
+# Set ugems listed in the Gemfile.
 # See: http://gembundler.com/bundler_setup.html
 #      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
--- public/js/application.js
+++ public/js/application.js
@@ -1,22 +1,20 @@
 function letterColor(element, newColor){
   var fadeTime = 200;
   return $(element).animate({color: newColor}, fadeTime, 'linear')
-}
+};
 
 var randomColor = function(){
   return "#" + Math.floor(Math.random()*16777215).toString(16);
-}
+};
 
 function letterClick(element){
   var letter      = element.innerText.toLowerCase();
   var indexLetter = $( ".header li" ).index(element) + 1;
   var leadLetter  = indexLetter < 7 ? 'f' : indexLetter === 14 ? 'l2' : 'l';
-
-
   return $.get("letters/" + leadLetter + letter, { letter: (leadLetter + letter) }, function(data){
    $('.container').html(data);
- })
-}
+ });
+};
 
 $(document).ready(function() {
 
@@ -33,5 +31,4 @@ $(document).ready(function() {
   $('.header li').click(function(){
     letterClick(this);
   });
-
 });
